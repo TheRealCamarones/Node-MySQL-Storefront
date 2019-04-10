bba61ebd277e08f4bcc7b9fe48bdeb7c13ecb4f8
Camarones <cameron_walther@yahoo.com>
2019-04-10 16:39:29 -0400

    working through an error on the inventory display, but almost finished with customer view basic functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,11 +14,54 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-
-    connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-        console.log(res);
-    })
-
-    connection.end();
+    start();
 });
+
+function start() {
+    connection.query("SELECT id, product_name, price FROM products", function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(`
+------------------------------------------------------------------------------------------
+ID: ${res[i].id} | ${res[i].product_name} | Price: ${res[i].price}`)}
+userPrompt();
+    })
+}
+
+function userPrompt() {
+    inquirer
+      .prompt([
+          {
+          name: "id",
+          type: "input",
+          message: "Which id number would you like to purchase?",
+          validate: function(value) {
+              if (isNaN(value) === false) {
+                  return true;
+              }
+              return false;
+          }},
+          {
+          name: "quantity",
+          type: "input",
+          message: "How many would you like to buy?",
+          validate: function(value) {
+              if (isNaN(value) === false) {
+                  return true;
+              }
+              return false;
+          }
+        }
+    ])
+    .then(function(answer) {
+        connection.query("SELECT * FROM products WHERE ")
+        if (res[answer.id].stock_quantity >= answer.quantity) {
+            console.log(`
+Congratulations. You've purchased ${answer.quantity} ${res[answer.id].product_name}(s)`)
+        }
+        else {
+            console.log(`
+Insufficient Quantity. We only have ${res[answer.id].stock_quantity} ${res[answer.id].product_name} in stock.`)
+        }
+    })
+}
